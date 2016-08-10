e4287af6d18af423d63eba9730fbb0891b30d025
foxulee <foxulee@163.com>
2016-08-10 16:44:32 -0400

    This is my CSharp learning progress!

---
+++ CSharp learning/test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, this is my first time to use GitHub!");
+            Console.ReadKey();
+        }
+    }
+}
